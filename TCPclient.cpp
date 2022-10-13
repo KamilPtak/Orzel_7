522a54e81b0d05495674e59409a3f4295b5db697
Tajajker <tajajker@gmail.com>
2022-10-13 11:43:49 -0700

    3

---
+++ TCPclient.cpp
@@ -0,0 +1,4 @@
+// https://www.geeksforgeeks.org/socket-programming-cc/
+
+//Odbieranie komunikaktow z PC - przesyl danych diagnostycznych w drug strone
+//TCP Server na PC - TCP Client na rpi
--- motorControl.hpp
+++ motorControl.hpp
@@ -1,12 +1,38 @@
 #include <vector>
+#include <string>
 
 #pragma once
 
+class Vechicle {
+    private:
+        Motor rightMotor;
+        Motor leftMotor;
+
+    public:
+        Vechicle();
+
+        void move(std::string direction);
+        void moveForward();
+        void moveLeft();
+        void moveRight();
+};
+
 class Motor {
     private:
-        int speed;
-        std::vector<int> pinout;
-    public:
-        Motor();
+        int speed; // PWM 0-100
+        uint8_t enable, dir1, dir2; // numery pinow do mostka H
 
+    public:
+        Motor(uint8_t enable_, uint8_t dir1_, uint8_t dir2_);
+
+        void setSpeed(int speed_);
+        void stop();
+
+};
+
+
+//TODO - klasa akcelerometru 
+class Accelerometr {
+    private:
+    public:
 };
