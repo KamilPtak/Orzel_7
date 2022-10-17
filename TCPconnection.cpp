11649d860fc9eac59032428a26955527e8be7c7d
Tajajker <tajajker@gmail.com>
2022-10-17 07:10:36 -0700

    5

--- TCPconnection.cpp
+++ TCPconnection.cpp
@@ -3,21 +3,36 @@
 #include "TCPconnection.hpp"
 #include "myExceptions.hpp"
 
-void TCP::createSocket() {
-    sockfd = socket(AF_INET , SOCK_STREAM , 0);
-
-    if (sockfd < 0){
-        throw TCPException("Error while creating TCP socket");
-    }
-
-    if(setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
-        throw TCPException("Jak tu sie sypie to usunac tego ifa");
-    }
-}
-
 void TCP::setAddress() {
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8000);
 }
 
+void TCP::createSocket() {
+    serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverfd < 0){
+        throw TCPException("Error while creating TCP socket");
+    }
+}
+
+void TCP::setSockOpt() {
+    if(setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
+        throw TCPException("Jak tu sie sypie to usunac tego ifa");
+    }
+}
+
+void TCP::bindSocket() {
+        if(bind(serverfd, (sockaddr*) &address, sizeof(address)) < 0) {
+        throw TCPException("Error while binding socket");
+    }
+}
+
+
+
+void TCP::createConnection() {
+    setAddres();
+    createSocket();
+    setSockOpt();
+    bindSocket();
+}
--- TCPconnection.hpp
+++ TCPconnection.hpp
@@ -5,19 +5,24 @@
 
 class TCP {
     private:
-        int sockfd, serverfd, newSocket, valread;
+        int serverfd, newSocket, valread;
         sockaddr_in address;
         int opt = 1;
         int addrlen = sizeof(address);
 
         char buffer[1024] = {0};
-        char* hello = "Hello from server";
+
+        void setAddres();
+        void createSocket();
+        void setSockOpt();
+        void bindSocket();
+        void setAddress();
     
     public:
-        TCP(): sockfd(-1) {}
+        TCP();
 
-        void createSocket();
-        void setAddress();
+        void createConnection();
+
         std::string sendData(std::string &data);
         void receiveData();
 
