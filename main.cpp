2af5e9818e7380fc4e24d241cb8ee5cfa334a1c6
Tajajker <tajajker@gmail.com>
2022-10-21 01:15:33 -0700

    modified:   TCPconnection.cpp     modified:   TCPconnection.hpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
+
 #include "TCPconnection.hpp" //przy rozszeeniu .cpp jakims cudem dziala - teraz nie widzi definicji funkcji z tcp.cpp
 #include "userTypeDefs.hpp"
 
 PORT port;
 
 
+
 int main(int argc, char *argv[]) {
 
-
-
     if (argc != 2) {
-        //Nie sprecyzowany port - rzucenie wyjtku do otwarcia 
-        //komunikacji na standardowym porcie
-        std::cerr<< "Run program at port 80000. To specify port run 'program <port>'\n";
-        port = 80000;
+        std::cerr<< "Program launched on port 80000. To specify port run 'program <port>'\n";
+        port  = 80000;
+    }
+    else {
+        port  = atoi(argv[1]);
+        std::cerr<<"The program launched on port "<< port<<"\n";
     }
 
-    // TCP* tcp = new TCP(port);
-    TCP tcp(port);
-    tcp.createConnection();
-    tcp.closeConnection();
-    // delete tcp;
+
+    TCP* tcp = new TCP(port);
+    try{
+        tcp->createConnection();
+    }
+    catch (TCPException& e){
+        std::cerr<<e.what()<<"\n";
+        tcp ->closeConnection();
+        delete tcp;    
+    }
+
+
+
+    tcp->closeConnection();
+    delete tcp;    
+    
 
     std::cout<<"dupa"<<std::endl;
     return 0;
