aa9cd90ee926a9218f008a56aa339bba48bbd5f2
Tajajker <tajajker@gmail.com>
2022-10-29 04:57:23 -0700

    Prepare motor data to send

--- inc/motor.hpp
+++ inc/motor.hpp
@@ -1,29 +1,28 @@
 #include <vector>
 #include <string>
+
 #pragma once
 
 class Motor {
     private:
         int speed; // PWM 0-100
-        uint8_t enable, dir1, dir2; 
+        bool dir1, dir2; 
         const uint8_t enablePin, dir1Pin, dir2Pin; // numery pinow do mostka H
 
+        static uint8_t motorID;
+        uint8_t id;
     public:
-        Motor(): enablePin(0), dir1Pin(0), dir2Pin(2) {}
+        Motor();
         Motor(uint8_t enable_, uint8_t dir1_, uint8_t dir2_);
 
         void setSpeed(int speed_);
         void stop(); // Emergency stop
         void turnForward();
         void turnBack();
+        uint8_t getMotorID() {return id;}
+
+        inline const std::string boolToString(bool b);
+        std::string packDataToSend();
 
 };
-
-
-//TODO - klasa akcelerometru 
-// class Accelerometr {
-//     private:
-//     public:
-// };
-
 
