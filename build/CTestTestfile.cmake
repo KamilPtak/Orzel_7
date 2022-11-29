5192e3952cda4f0d02984ad4c9a92291166e9bf5
KamilPtak <57808303+KamilPtak@users.noreply.github.com>
2022-11-29 11:25:42 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kamil/Desktop/Orzel_7
-# Build directory: /home/kamil/Desktop/Orzel_7/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
