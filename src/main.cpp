5c80b6fbeb04ba753fca56a185b88db3959f3f34
KamilPtak <tajajker@gmail.com>
2023-01-20 08:20:27 -0800

    fix some bugs, better printing

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,19 @@
 #define LISTENER 1
 uint port = 80002;
 
-void runRobotRun(std::pair<std::string, Vechicle*> inputPair);
-
 int main(int argc, char *argv[]) {
 
     if (argc != 2) {
+        std::cerr<<"\n"<<"!==============================!"<<"\n";
         std::cerr<< "Program launched on port "<<port
                 <<". To specify port run 'program <port>'\n\n";
         std::cerr<<"!=============NOTE=============!"<<"\n";
-        std::cerr<<"To set robot speed please use '_' instad od space."<<"\n\n";
+        std::cerr<<"To set robot speed please use '_' instad of space."<<"\n\n";
 
     }
     else {
         port  = atoi(argv[1]);
+        std::cerr<<"\n"<<"!==============================!"<<"\n";
         std::cerr<<"The program launched on port "<<port<<"\n\n";
         std::cerr<<"!=============NOTE=============!"<<"\n";
         std::cerr<<"To set robot speed please use '_' instad od space."<<"\n\n";
@@ -44,17 +44,18 @@ int main(int argc, char *argv[]) {
             messageQueue.push(rcvData);
 
             if(messageQueue.size() != 0 ) {
-                auto msg = messageQueue.front();
+                std::string msg = messageQueue.front();
                 messageQueue.pop();
-                vechicle->decodeMessageFromClient(rcvData);
+                vechicle->decodeMessageFromClient(msg);
             }
             else {
                 std::cout<<"Waiting for message"<<"\n";
                 usleep(200);
             }
         } while (rcvData != "close");
-        delete tcp;
-        delete vechicle;
+            vechicle->decodeMessageFromClient("stop");
+            delete tcp;
+            delete vechicle;
     }
     catch (Exception& e){
         std::cerr<<"Exception!!!"<<"\n"<<e.what()<<"\n";
@@ -62,18 +63,4 @@ int main(int argc, char *argv[]) {
 
     std::cerr<<"Thanks for a ride!!!"<<std::endl;
     return 0;
-}
-
-
-void runRobotRun(std::pair<std::string, Vechicle*> inputPair) {
-    std::string msg = inputPair.first;
-    Vechicle* vechicle = std::move(inputPair.second);
-    std::cout<<"I am child thread!!!"<<std::endl;
-
-    try{
-        vechicle -> decodeMessageFromClient(msg);
-    }
-    catch (Exception& e){
-        std::cerr<<"Exception!!!"<<"\n"<<e.what()<<"\n";
-    }
 }
